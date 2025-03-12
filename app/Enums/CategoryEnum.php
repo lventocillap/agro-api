d26bb6dc961935cad5007b83e9af056f1acc736c
Luis Fernando Ventocilla Polin <1507430@senati.pe>
2025-03-12 15:50:44 -0500

    creación de entidades de categoria y subcategoria

---
+++ app/Enums/CategoryEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+enum CategoryEnum: string
+{
+    case CAT_01 = 'Especialidades';
+    case CAT_02 = 'Nutrición de cultivos';
+    case CAT_03 = 'Protección de cultivos';
+    case CAT_04 = 'Avicultura';
+    case CAT_05 = 'Bioseguridad';
+    case CAT_06 = 'Ganaderia';
+    case CAT_07 = 'Porcicultura';
+}
