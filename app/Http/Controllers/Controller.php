d94ae4f1d4be9e30aa16dc6324bfd7a1b4156416
josephgonzales2025 <josephacuario20@gmail.com>
2025-03-20 17:34:22 -0500

    Modificando Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,28 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-<<<<<<< HEAD
-/**
- * @OA\Info(
- *     title="Laravel 11 API Documentation",
-=======
 
 /**
- * @OA\Info(
- *     title="Agro API",
->>>>>>> main
- *     version="1.0.0",
- *     description="Documentación de la API de autenticación con Swagger en Laravel 11",
- *     @OA\Contact(
- *         email="soporte@tuempresa.com"
- *     )
- * )
- * 
- * @OA\Server(
- *     url="http://localhost:8000",
- *     description="API Server"
- * )
- */
+ 
+@OA\Info(
+title="Agro API",
+version="1.0.0",
+description="Documentación de la API de autenticación con Swagger en Laravel 11",
+@OA\Contact(
+email="soporte@tuempresa.com"
+)
+)
+@OA\Server(
+url="http://localhost:8000/",
+description="API Server"
+)*/
 abstract class Controller
 {
     //
