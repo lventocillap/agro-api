e9d284c4c0ffeab116d2b4beab1752b2cdb9d61a
Luis Fernando Ventocilla Polin <1507430@senati.pe>
2025-03-24 20:01:13 -0500

    borrar pdf

--- app/Http/Controllers/Pdf/PdfController.php
+++ app/Http/Controllers/Pdf/PdfController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Pdf;
 
 use App\Exceptions\Product\NotFoundProduct;
 use App\Http\Controllers\Controller;
+use App\Http\Service\PDF\DeletePDF;
 use App\Http\Service\PDF\SavePDF;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
@@ -11,13 +12,14 @@ use Illuminate\Http\Request;
 
 class PdfController extends Controller
 {
-    use SavePDF;
+    use SavePDF, DeletePDF;
     public function pdfUpdateProduct(string $nameProduct, Request $request): JsonResponse
     {
         $product = Product::where('name', $nameProduct)->first();
         if(!$product){
             throw new NotFoundProduct;
         }
+        $this->deletePDF($product->pdf->url);
         $pdf = $this->savePDFBase64($request->pdf);
         $product->pdf()->update([
             'url' => $pdf,
